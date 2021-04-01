31f5dadb45ad2a15fcd0d6389e3902ff028986b1
almira_inez <fortunainez@gmail.com>
2021-04-01 10:53:19 +0700

    change cut off at config 31 days

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -1113,6 +1113,8 @@ class AttendanceController extends Controller
                     $getbreakworkingtime = getBreaktimeWorkingtime($breaktimes, $attendance_hour, $getworkingtime); //menghitung total istirahat jam kerja
                     $getbreakovertime = getBreaktimeOvertime($breaktimes, $attendance_hour, $getworkingtime); //menghitung istirahat jam lembur
 
+                    // dd($getbreakworkingtime, $breaktimes, $attendance_hour, $getworkingtime);
+
                     $workhour = $getworkingtime->workhour;
                     $min_workhour = $getworkingtime->min_workhour;
                     //overtime
@@ -1146,6 +1148,7 @@ class AttendanceController extends Controller
                         $break_time = $getbreakworkingtime + $getbreakovertime;
                         // WT
                         $wt_attendance = $total_jam - $break_time;
+                        
 
                         if ($adjustment->day == 'Off') {
                             // Check timeout
@@ -1324,20 +1327,19 @@ class AttendanceController extends Controller
                                 } 
                             }
                         } else {
+
+                            // dd([
+                            //     'out', $adjustment->attendance_out, 'in', $adjustment->attendance_in, 'total jam', $total_jam, 'breakWT', $getbreakworkingtime,
+                            //     'breakOT', $getbreakovertime, 'breaktime', $break_time, 'wt_attendance', $wt_attendance, 'min_workhour', $min_workhour
+                            // ]);
                             
-                            $total_jam = Carbon::parse($adjustment->attendance_out)->diffInHours($adjustment->attendance_in);
-
-                            $break_time = $getbreakworkingtime + $getbreakovertime;
-
-                            $wt_attendance = $total_jam - $break_time;
-                            
-                            $wt = ($adj_working_time == $min_workhour) ? $adj_working_time : $adj_working_time - $getbreakworkingtime;
-
                             if ($employee->timeout == 'yes') {
                                 if ($adjustment->attendance_in && $adjustment->attendance_out) {
 
                                     if ($employee->overtime == 'yes') {
                                         $ot = $wt_attendance - $min_workhour;
+
+                                        dd($ot);
 
                                         if ($employee->spl == 'yes') {
                                             $cekspl = Spl::where('spl_date', $adjustment->attendance_date)->where('employee_id', $employee->id)->first();
