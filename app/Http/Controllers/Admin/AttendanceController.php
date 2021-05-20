7350c688ab4b566d698e30120357ac41150e4ea8
almira_inez <fortunainez@gmail.com>
2021-05-20 11:35:06 +0700

    fixing bug

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -2218,16 +2218,16 @@ class AttendanceController extends Controller
                 $checkOutNextDay= AttendanceLog::whereBetween('attendance_date', [$attendanceIn->toDateString(), $cutOutNextDay->toDateString()])->where('employee_id', $attendance->employee_id)->where('type', 0)->get();
                 if ($checkOutNextDay->count() > 0) {
                     $attendanceOut      = $checkOutNextDay->max('attendance_date');
-                    // if (!$attendanceOut) {
-                    //     return response()->json([
-                    //         'status'    => false,
-                    //         'message'   => $attendanceOut,
-                    //     ], 400);
-                    // }
-                    // foreach ($attendanceOut as $key => $out) {
-                    $attendanceOut->attendance_id     = $attendance->id;
-                    $attendanceOut->save();
-                    // }
+                    if (!$attendanceOut) {
+                        return response()->json([
+                            'status'    => false,
+                            'message'   => "Error cant get attendance out"
+                        ], 400);
+                    }
+                    foreach ($checkOutNextDay as $key => $out) {
+                        $out->attendance_id     = $attendance->id;
+                        $out->save();
+                    }
                 } else {
                     $attendanceOut      = null;
                 }
