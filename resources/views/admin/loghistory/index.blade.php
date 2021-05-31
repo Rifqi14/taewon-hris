5669f59dbf9a54707b5bfe46db0410a99ba69522
baguzwahyu <baguzwahyu7@gmail.com>
2021-05-31 13:55:08 +0700

    log history

--- resources/views/admin/loghistory/index.blade.php
+++ resources/views/admin/loghistory/index.blade.php
@@ -162,7 +162,6 @@
         { className: "text-center", targets: [0,1,3,4,5,6] },
         {
           targets: [7],
-          visible: false,
         },
       ],
       columns: [
@@ -173,7 +172,7 @@
         { data: "name" },
         { data: "department_name" },
         { data: "activity" },
-        { data: "activity"},
+        { data: "detail"},
         { data: "result"},
       ]
     });
