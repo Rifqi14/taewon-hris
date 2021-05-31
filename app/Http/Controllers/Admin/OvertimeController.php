e034645c90980f3447d5685326da7a54e0eefbb7
baguzwahyu <baguzwahyu7@gmail.com>
2021-05-31 17:07:06 +0700

    log history overtimes

--- app/Http/Controllers/Admin/OvertimeController.php
+++ app/Http/Controllers/Admin/OvertimeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Employee;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class OvertimeController extends Controller
 {
