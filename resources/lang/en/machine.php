2b6a571637cb26b6703dce2054280f134a846b87
Rifqi14 <rifqi.persie@gmail.com>
2021-06-02 12:34:28 +0700

    Change in localization machine

--- resources/lang/en/machine.php
+++ resources/lang/en/machine.php
@@ -3,5 +3,7 @@ return [
     'machine'=> 'Attendance Machine',
     'malist' => 'Attendance Machine List',
     'device' => 'Device Serial Number',
-    'point'  => 'Point Name'
+    'point'  => 'Point Name',
+    'in'     => 'IN',
+    'out'    => 'OUT',
 ];
