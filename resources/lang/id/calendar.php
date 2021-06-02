2cbefa45ccf50cc422debdc6727881e69c2cd766
Rifqi14 <rifqi.persie@gmail.com>
2021-06-02 09:23:26 +0700

    Change calender translate

--- resources/lang/id/calendar.php
+++ resources/lang/id/calendar.php
@@ -1,16 +1,17 @@
 <?php
 return[
     /**Working Calendar */
-    'calendar'      => 'Kalendar',
-    'calcode'       => 'Kode Kalendar',
-    'calname'       => 'Nama Kalendar',
-    'labelclr'      => 'Label Warna',
+    'calendar'      => 'Kalender',
+    'calcode'       => 'Kode Kalender',
+    'calname'       => 'Nama Kalender',
+    'labelclr'      => 'Warna Label',
     'desc'          => 'Deskripsi',
     'listex'        => 'Daftar Hari Libur',
     'recday'        => 'Pengulangan Hari',
     'specday'       => 'Hari Tertentu',
     'rangerec'      => 'Periode',
     'swcday'        => 'Pergantian Hari',
+    'textclr'       => 'Warna Teks',
     'start_date'    => 'Tanggal Mulai',
     'finish_date'   => 'Tanggal Selesai',
     'exception'     => 'Hari Libur',
