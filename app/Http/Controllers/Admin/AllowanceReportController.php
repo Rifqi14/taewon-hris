b9556f0e53ba42ebd8fa8cdfb5739c4ca5db764a
Jefri Rendra <jefrirendra@Jefris-MacBook-Pro.local>
2021-06-03 11:02:35 +0700

    Index

--- app/Http/Controllers/Admin/AllowanceReportController.php
+++ app/Http/Controllers/Admin/AllowanceReportController.php
@@ -139,6 +139,13 @@ class AllowanceReportController extends Controller
     }
     if ($departments) {
       $string = '';
+      $uniqdepartments = [];
+      foreach($departments as $department){
+          if(!in_array($department,$uniqdepartments)){
+              $uniqdepartments[] = $department;
+          }
+      }
+      $departments = $uniqdepartments;
       foreach ($departments as $department) {
         $string .= "departments.path like '%$department%'";
         if (end($departments) != $department) {
