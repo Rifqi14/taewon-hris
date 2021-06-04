889c8566fd42df41d9a8cf84ba83e636c7d8629b
almira_inez <fortunainez@gmail.com>
2021-06-04 11:12:07 +0700

    multi language activity log

---
+++ resources/lang/id/activity.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'aclog'   => 'Catatan Aktivitas',
+    'listaclog'=> 'Daftar Catatan Aktivitas',
+    'user'    => 'Pengguna',
+    'page'    => 'Halaman',
+    'activity'=> 'Aktivitas',
+    'from'    => 'Dari',
+    'to'      => 'Sampai',
+    'rslt'    => 'Hasil',
+];
