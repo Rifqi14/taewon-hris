215acb8637a7badb0b6512446228521c03a51fe1
Jefri Rendra <jefrirendra@Jefris-MacBook-Pro.local>
2021-06-08 18:14:51 +0700

    Delivery Order

--- app/Http/Controllers/Admin/DeliveryOrderController.php
+++ app/Http/Controllers/Admin/DeliveryOrderController.php
@@ -733,7 +733,7 @@ class DeliveryOrderController extends Controller
                         DB::rollback();
                         return response()->json([
                             'status' => false,
-                            'message'   => $rit
+                            'message'   => 'Please check your driver allowance'
                         ], 400);
                     }
                     $checkupdate->rit = $driverlist->value;
