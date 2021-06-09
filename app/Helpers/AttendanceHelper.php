40d46ab9a2e0bfe2b1f1bc9b55195254c323453d
Jefri Rendra <jefrirendra@Jefris-MacBook-Pro.local>
2021-06-09 12:02:43 +0700

    Attendance

--- app/Helpers/AttendanceHelper.php
+++ app/Helpers/AttendanceHelper.php
@@ -62,9 +62,11 @@ if (!function_exists('calculateAttendance')) {
     if ($attendance->attendance_in) {
       if (($workingtime->start >= changeDateFormat('H:i:s', $attendance->attendance_in)) && (changeDateFormat('H:i:s', $attendance->attendance_in) >= $workingtime->min_in)) {
           $start_shift = changeDateFormat('Y-m-d H:i:s', changeDateFormat('Y-m-d', $attendance->attendance_in) . ' ' . $workingtime->start);
-          $totalattendance = Carbon::parse($attendance->attendance_out)->diffInHours($start_shift);
+          //$totalattendance = Carbon::parse($attendance->attendance_out)->diffInHours($start_shift);
+          $totalattendance = roundedTime(countWorkingTime($start_shift, $attendance->attendance_out));
       } else {
-          $totalattendance = Carbon::parse($attendance->attendance_out)->diffInHours($attendance->attendance_in);
+          //$totalattendance = Carbon::parse($attendance->attendance_out)->diffInHours($attendance->attendance_in);
+          $totalattendance = roundedTime(countWorkingTime($attendance->attendance_in, $attendance->attendance_out));
       }
       //$totalbreaktime = $breakworkingtime + $breakovertime;
       $totalbreaktime = $breakall;
