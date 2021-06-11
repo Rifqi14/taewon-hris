f6f2796d1299f798f41fdc26ce5f59805d1113be
Jefri Rendra <jefrirendra@Jefris-MacBook-Pro.local>
2021-06-11 11:06:54 +0700

    Salary Report

--- app/Helpers/AttendanceHelper.php
+++ app/Helpers/AttendanceHelper.php
@@ -62,11 +62,11 @@ if (!function_exists('calculateAttendance')) {
     if ($attendance->attendance_in) {
       if (($workingtime->start >= changeDateFormat('H:i:s', $attendance->attendance_in)) && (changeDateFormat('H:i:s', $attendance->attendance_in) >= $workingtime->min_in)) {
           $start_shift = changeDateFormat('Y-m-d H:i:s', changeDateFormat('Y-m-d', $attendance->attendance_in) . ' ' . $workingtime->start);
-          $totalattendance = Carbon::parse($attendance->attendance_out)->diffInHours($start_shift);
-          //$totalattendance = roundedTime(countWorkingTime($start_shift, $attendance->attendance_out));
+          //$totalattendance = Carbon::parse($attendance->attendance_out)->diffInHours($start_shift);
+          $totalattendance = roundedTime(countWorkingTime($start_shift, $attendance->attendance_out));
       } else {
-          $totalattendance = Carbon::parse($attendance->attendance_out)->diffInHours($attendance->attendance_in);
-          //$totalattendance = roundedTime(countWorkingTime($attendance->attendance_in, $attendance->attendance_out));
+          //$totalattendance = Carbon::parse($attendance->attendance_out)->diffInHours($attendance->attendance_in);
+          $totalattendance = roundedTime(countWorkingTime($attendance->attendance_in, $attendance->attendance_out));
       }
       //$totalbreaktime = $breakworkingtime + $breakovertime;
       $totalbreaktime = $breakall;
@@ -299,13 +299,13 @@ if (!function_exists('calculateAttendance')) {
                 $attendance->adj_over_time = $totalovertime;
                 $attendance->adj_working_time = $totalworkingtime - $totalovertime;
                 $attendance->code_case  = "A19/BW$breakworkingtime/BO$breakovertime";
-                $attendance->note = json_encode(array(
-                   'totalattendance'=>$totalattendance,
-                   'totalworkingtime'=>$totalworkingtime,
-                   'totalovertime'=>$totalovertime,
-                   'totalbreaktime'=>$totalbreaktime,
+                // $attendance->note = json_encode(array(
+                //    'totalattendance'=>$totalattendance,
+                //    'totalworkingtime'=>$totalworkingtime,
+                //    'totalovertime'=>$totalovertime,
+                //    'totalbreaktime'=>$totalbreaktime,
 
-                ));
+                // ));
                 $attendance->breaktime = $totalbreaktime;
             }
         } else {
