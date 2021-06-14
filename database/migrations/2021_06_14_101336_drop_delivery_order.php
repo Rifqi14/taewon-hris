bb4c79d502fa2f1be7fd58205cae2cc866aa770c
almira_inez <fortunainez@gmail.com>
2021-06-14 10:23:04 +0700

    do

---
+++ database/migrations/2021_06_14_101336_drop_delivery_order.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropDeliveryOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('driver_allowance_lists', function (Blueprint $table) {
+            $table->dropForeign(['delivery_order_id']);
+            $table->dropColumn('delivery_order_id');
+        });
+        Schema::table('driver_allowance_lists', function (Blueprint $table) {
+            $table->unsignedBigInteger('delivery_order_id')->nullable();
+            $table->foreign('delivery_order_id')->references('id')->on('delivery_orders')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('driver_allowance_lists', function (Blueprint $table) {
+            $table->unsignedBigInteger('delivery_order_id')->nullable();
+            $table->foreign('delivery_order_id')->references('id')->on('driver_allowance_lists')->onDelete('cascade')->onUpdate('cascade');
+        });
+        Schema::table('driver_allowance_lists', function (Blueprint $table) {
+            $table->dropForeign(['delivery_order_id']);
+            $table->dropColumn('delivery_order_id');
+        });
+    }
+}
