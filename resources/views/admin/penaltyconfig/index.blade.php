02ef3aa4a01609fd476d7429d8221b54d99efcc2
Jefri Rendra <jefrirendra@Jefris-MacBook-Pro.local>
2021-07-12 16:57:47 +0700

    Penalty COnfig

--- resources/views/admin/penaltyconfig/index.blade.php
+++ resources/views/admin/penaltyconfig/index.blade.php
@@ -44,7 +44,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title">{{ __('general.filter') }}</h4>
+        <h4 class="modal-title">{{ __('general.srch') }}</h4>
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
       </div>
       <div class="modal-body">
