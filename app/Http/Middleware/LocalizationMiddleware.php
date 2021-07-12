bae36689a28209878a7c64107f5e26be0981d100
Jefri Rendra <jefrirendra@Jefris-MacBook-Pro.local>
2021-07-12 15:01:21 +0700

    Localization

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -19,8 +19,13 @@ class LocalizationMiddleware
     public function handle($request, Closure $next)
     {
        
-        $request->session()->put('locale', Auth::guard('admin')->user()->language);
-        App::setLocale($request->session()->get('locale'));
+        if(Auth::guard('admin')->check()){
+            $request->session()->put('locale', Auth::guard('admin')->user()->language?Auth::guard('admin')->user()->language:'en');
+            App::setLocale($request->session()->get('locale'));
+        }
+        else{
+            App::setLocale('en');
+        }
         // Lanjutkan request.
         return $next($request);
     }
